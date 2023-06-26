9b1558ffa1c21c94b9568fc9a2778c8cd9c56b48
HelloJon29 <jonkim1996@gmail.com>
2023-06-26 08:50:20 -0500

    First Python code! Hello World and some basic rules

---
+++ hello_world/hello_world.py
@@ -0,0 +1,5 @@
+print("Hello World") #print() function and no need for semi colons in python
+print("o----")
+print(" ||||") # Little dog drawing, the interperter executes line by line
+
+print("*" * 10) # using an expression here, python recognizes the data type automatically, in this case "*"" is a string and printed 10 times
