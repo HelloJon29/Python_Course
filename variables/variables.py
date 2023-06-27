40f1440766424465b51f6fa80a13060c49fccbfe
HelloJon29 <jonkim1996@gmail.com>
2023-06-27 07:50:01 -0500

    Example of simple variables in Python

---
+++ variables/variables.py
@@ -0,0 +1,7 @@
+price = 10
+rating = 4.9 # integer and float types
+name = 'Jon' # string type, single and double quotes work
+is_published = False #Python is case sensistive, True or Flase boolean values must be upper case or an error will occur
+# the above are examples of storing simple values
+
+print(price)
