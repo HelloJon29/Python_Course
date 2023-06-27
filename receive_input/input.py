2978fc698126e902f4ccebf0792b2439fd052509
HelloJon29 <jonkim1996@gmail.com>
2023-06-27 07:56:37 -0500

    Example of getting user input with input() function

---
+++ receive_input/input.py
@@ -0,0 +1,3 @@
+name = input('What is your name? ') #Parenthesis means calling the function or executing it
+color = input('What is your fav color? ')
+print ('Hi ' + name + ' I also like ' + color) # concatinating string with variable from the input() functions called earlier
