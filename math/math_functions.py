531437b5dd1626fa15c56dda0b856bd50df681b9
HelloJon29 <jonkim1996@gmail.com>
2023-06-30 11:06:01 -0500

    Added math operations and functions examples

---
+++ math/math_functions.py
@@ -0,0 +1,11 @@
+import math # this how to import modules, making math an object in this file
+x = 2.6 
+print(round(x)) # round function will round up .6, and round down .5
+
+print(abs(-2.6)) # this is absolute function, always return the positive value
+
+print(math.ceil(2.9)) # this is example of using . operator on math object to access it's methods
+print(math.floor(2.9))
+
+# https://docs.python.org/3/library/math.html
+# docs for math module
---
+++ math/operators.py
@@ -0,0 +1,16 @@
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 % 3) # all math operators
+print(2 ** 2) # this is an exponent
+
+# example of incrementing
+x = 10
+x = x + 3 
+x += 2 # this is the same as line 9, can also decrement the same way
+print(x)
+
+# Order of operations is present in python
+y = 10 + 3 * 2 # PEMDAS y = 16
+print(y)
